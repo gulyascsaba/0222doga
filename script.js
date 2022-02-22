81905fae6a162907e321393a275ffa6dc28497fe
Csaba <gulyasscsaba@gmail.com>
2022-02-22 13:09:23 +0100

    js

---
+++ script.js
@@ -0,0 +1,19 @@
+function Nyom(){
+
+
+let pont=0;
+if(document.getElementById("2").checked==true){
+    pont++;
+}
+
+if(document.getElementById("Steak").checked==true){
+    pont++;
+}
+if(document.getElementById("Rántott").checked==true){
+    pont++;
+}
+
+
+
+
+}

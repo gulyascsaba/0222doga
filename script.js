7968979b526a8133e44554f283b11175331aeb55
Csaba <gulyasscsaba@gmail.com>
2022-02-22 13:21:08 +0100

    Vegso valtoztatasok

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ if(document.getElementById("2").checked==true){
 if(document.getElementById("Steak").checked==true){
     pont++;
 }
-if(document.getElementById("Rántott").checked==true){
+if(document.getElementById("Rantott").checked==true){
     pont++;
 }
-
-
-
+if(document.getElementById("varos").value=="Igaz"){
+    pont++;
+}
+if(document.getElementById("egyes").value=="2022"){
+    pont++;
+}
+if(document.getElementById("kettes").value=="65"){
+    pont++;
+}
+document.getElementById("valaszok").innerHTML=pont+" pontod lett az 5 kérdésből";
 
 }
